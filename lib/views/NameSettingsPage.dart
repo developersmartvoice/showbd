86cd672a794747ec2c0af161dbb7eaa0a67cde1c
Shibam Dey <shibam.dey.sd@gmail.com>
2024-02-18 10:35:33 +0600

    Some UI need to change.

--- lib/views/NameSettingsPage.dart
+++ lib/views/NameSettingsPage.dart
@@ -44,143 +44,146 @@ class NameSettingsPage extends StatefulWidget {
 class _NameSettingsPageState extends State<NameSettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Name',
-          style: GoogleFonts.robotoCondensed(
-            color: Colors.white,
-            fontSize: 25,
-            fontWeight: FontWeight.w700,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Name',
+            style: GoogleFonts.robotoCondensed(
+              color: Colors.white,
+              fontSize: 25,
+              fontWeight: FontWeight.w700,
+            ),
           ),
-        ),
-        backgroundColor: const Color.fromARGB(255, 243, 103, 9),
-        centerTitle: true,
-        actions: [
-          TextButton(
-            onPressed: () {
-              // Handle button tap
-            },
-            child: Text(
-              'Save',
-              style: GoogleFonts.robotoCondensed(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
+          backgroundColor: const Color.fromARGB(255, 243, 103, 9),
+          centerTitle: true,
+          actions: [
+            TextButton(
+              onPressed: () {
+                // Handle button tap
+              },
+              child: Text(
+                'Save',
+                style: GoogleFonts.robotoCondensed(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 70,
-            color: Colors.white,
-            child: Stack(
-              children: [
-                Positioned(
-                  left: 10, // Adjust the position of the button as needed
-                  top: 20, // Adjust the position of the button as needed
-                  child: InkWell(
-                      //onTap: _changeColor,
-                      // Add your logic for the selection button onTap event here
+          ],
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 70,
+              color: Colors.white,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 10, // Adjust the position of the button as needed
+                    top: 20, // Adjust the position of the button as needed
+                    child: InkWell(
+                        //onTap: _changeColor,
+                        // Add your logic for the selection button onTap event here
 
-                      // child: Container(
-                      //   width: 30,
-                      //   height: 30,
-                      //   decoration: BoxDecoration(
-                      //     //color: _boxColor, // Color of the button
-                      //     color: Colors.green,
-                      //     shape: BoxShape.circle,
-                      //     border: Border.all(
-                      //       color: Colors.black, // Color of the border
-                      //       width: 1.0, // Width of the border
-                      //     ), // Circular shape
-                      //   ),
-                      //   child: Icon(
-                      //     Icons.check,
-                      //     //color: _isSelected ? Colors.green : Colors.white,
-                      //     color: Colors.white, // Color of the icon
-                      //     size: 25.0, // Size of the icon
-                      //   ),
-                      // ),
-                      ),
-                ),
-                Container(
-                  color: LIGHT_GREY_SCREEN_BACKGROUND,
-                  height: 70,
-                  child: Row(children: [
-                    Expanded(
-                      //child: Padding(
-                      //padding: const EdgeInsets.only(left: 15),
-                      child: Text(
-                        "Use your real first name",
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.robotoCondensed(
-                          fontSize: 15.0,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
+                        // child: Container(
+                        //   width: 30,
+                        //   height: 30,
+                        //   decoration: BoxDecoration(
+                        //     //color: _boxColor, // Color of the button
+                        //     color: Colors.green,
+                        //     shape: BoxShape.circle,
+                        //     border: Border.all(
+                        //       color: Colors.black, // Color of the border
+                        //       width: 1.0, // Width of the border
+                        //     ), // Circular shape
+                        //   ),
+                        //   child: Icon(
+                        //     Icons.check,
+                        //     //color: _isSelected ? Colors.green : Colors.white,
+                        //     color: Colors.white, // Color of the icon
+                        //     size: 25.0, // Size of the icon
+                        //   ),
+                        // ),
+                        ),
+                  ),
+                  Container(
+                    color: LIGHT_GREY_SCREEN_BACKGROUND,
+                    height: 70,
+                    child: Row(children: [
+                      Expanded(
+                        //child: Padding(
+                        //padding: const EdgeInsets.only(left: 15),
+                        child: Text(
+                          "Use your real first name",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.robotoCondensed(
+                            fontSize: 15.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                    //),
-                    // Align(
-                    //   alignment: Alignment.centerRight,
-                    //   child: IconButton(
-                    //     onPressed: () {
-                    //       // Add your logic for the onPressed event here
-                    //       // Typically, this would involve navigating to the next screen or performing some action
-                    //     },
-                    //     //alignment: Alignment.centerRight,
-                    //     icon: Icon(Icons.arrow_forward_ios_sharp),
-                    //     color: Colors.black, // Color of the icon
-                    //     iconSize: 24.0, // Size of the icon
-                    //   ),
-                    // ),
-                  ]),
-                ),
-              ],
+                      //),
+                      // Align(
+                      //   alignment: Alignment.centerRight,
+                      //   child: IconButton(
+                      //     onPressed: () {
+                      //       // Add your logic for the onPressed event here
+                      //       // Typically, this would involve navigating to the next screen or performing some action
+                      //     },
+                      //     //alignment: Alignment.centerRight,
+                      //     icon: Icon(Icons.arrow_forward_ios_sharp),
+                      //     color: Colors.black, // Color of the icon
+                      //     iconSize: 24.0, // Size of the icon
+                      //   ),
+                      // ),
+                    ]),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Divider(
-            height: 2,
-            color: Colors.grey,
-          ),
-          Container(
-            color: Colors.white,
-            height: 50,
-            child: Row(
-              children: [
-                Expanded(
-                  //child:
-                  //Padding(
-                  //padding: const EdgeInsets.only(right: 190.0),
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                    controller: TextEditingController(),
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ), // Use a TextEditingController
-                    decoration: InputDecoration(
-                      hintText: 'Name...',
-                      border: OutlineInputBorder(),
-                      hintStyle: TextStyle(
-                          color: Colors.grey), // Border around the input field
+            Divider(
+              height: 2,
+              color: Colors.grey,
+            ),
+            Container(
+              color: Colors.white,
+              height: 50,
+              child: Row(
+                children: [
+                  Expanded(
+                    //child:
+                    //Padding(
+                    //padding: const EdgeInsets.only(right: 190.0),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      controller: TextEditingController(),
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ), // Use a TextEditingController
+                      decoration: InputDecoration(
+                        hintText: 'Name...',
+                        border: OutlineInputBorder(),
+                        hintStyle: TextStyle(
+                            color:
+                                Colors.grey), // Border around the input field
+                      ),
                     ),
                   ),
-                ),
-                //),
-              ],
+                  //),
+                ],
+              ),
             ),
-          ),
-          Divider(
-            height: 2,
-            color: Colors.grey,
-          ),
-        ],
+            Divider(
+              height: 2,
+              color: Colors.grey,
+            ),
+          ],
+        ),
       ),
     );
   }
