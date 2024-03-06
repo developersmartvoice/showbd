687afd4a92c03b2b3d51a1026bae2e3e61eeb5bb
Tabassoom <developer.smartvoice@gmail.com>
2024-03-06 18:36:31 +0600

    Currency can be update.

--- lib/views/ShurjoPayPaymentGatewayScreen.dart
+++ lib/views/ShurjoPayPaymentGatewayScreen.dart
@@ -253,7 +253,7 @@ class _ShurjoPayPaymentState extends State<ShurjoPayPayment> {
                           style: TextStyle(fontSize: 18.0, color: Colors.white),
                         ),
                         style: ElevatedButton.styleFrom(
-                          primary: Colors.orange, // Change button color
+                          backgroundColor: Colors.orange, // Change button color
                           padding: EdgeInsets.symmetric(
                             vertical: 15.0,
                           ),
